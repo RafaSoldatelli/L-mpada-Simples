d5018c4f852fd121ee892a6f43e18a6a89a66d9c
Rafael Soldatelli da Rosa <105215757+RafaSoldatelli@users.noreply.github.com>
2023-01-03 08:37:05 -0300

    Update Lâmpada.js

--- Lâmpada.js
+++ Lâmpada.js
@@ -1,7 +1,9 @@
 function Ligar(){
+    // Alterar caminho 
     document.getElementById('lampada').src='D:\Programador Lifestyle/img/pic_bulbon.gif';
 }
 
 function Desligar(){
+    // Alterar caminho
     document.getElementById('lampada').src='D:\Programador Lifestyle/img/pic_bulboff.gif';
 }
